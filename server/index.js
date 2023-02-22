154aa7a4cd16ef274d54f41eaa3921858cbff89f
cybertronicEX <101054616+it20606138@users.noreply.github.com>
2023-02-22 13:55:50 +0530

    db connection

--- server/index.js
+++ server/index.js
@@ -1,5 +1,10 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const cors = require("cors");
+const mongoose = require ("mongoose");
+
+const app = express();
+
+mongoose.connect("mongodb+srv://group117:1234567abc@itpmcluster.lo7aecr.mongodb.net/test");
 
 app.listen(3001, ()=>{
     console.log("Server runs!")
