d02c8c9af39acbe69a44c2e3c796ecbfcae0c0b2
cybertronicEX <101054616+it20606138@users.noreply.github.com>
2023-02-22 22:56:12 +0530

    server router architecture

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -1,7 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export  default function Login () {
+    const [username,setusername]=useState('');
+    const [password,setpassword]=useState('');
+
+    async function UserLogin(event){
+        event.preventDefault();
+        
+    }
+
   return (
-    <div>login</div>
+    <div>
+        login
+        <form onSubmit={UserLogin}>
+           
+                <input 
+                    onChange={(e)=>setusername(e.target.value)}
+                    type='email' 
+                    required='true'
+                    value={username}
+                    placeholder="username"/>
+                    <br/>
+                <input
+                    type='password'
+                    required='true' 
+                    value={password} 
+                    placeholder='password'
+                    onChange={(e)=>setpassword(e.target.value)}/>
+            
+        </form>
+        <span></span>
+    </div>
   )
 }
